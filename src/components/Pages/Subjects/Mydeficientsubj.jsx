6f5e1fe72a785403ccb6d10d5a83b2036fa669fe
angelosoreta123 <angelosoreta12.as@gmail.com>
2024-04-23 13:35:00 +0800

    wews

--- src/components/Pages/Subjects/Mydeficientsubj.jsx
+++ src/components/Pages/Subjects/Mydeficientsubj.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import "./Mydeficientsubj.css";
-import { IoClose } from "react-icons/io5";
 
 const Mydeficientsubj = ({ selectedSubjects, removeSubject, className }) => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
-
   return (
     <div className={`defsubjwrapper ${className}`}>
       {selectedSubjects &&
@@ -13,16 +10,8 @@ const Mydeficientsubj = ({ selectedSubjects, removeSubject, className }) => {
             key={index}
             className="subject"
             onClick={() => removeSubject(subject.code, subject.name)}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <div className="defsubjectcode">
-              {hoveredIndex === index ? (
-                <IoClose className="closesign" />
-              ) : (
-                subject.code
-              )}
-            </div>
+            <div className="defsubjectcode">{subject.code}</div>
             <div className="defsubjectinfo">
               <h3>{subject.name}</h3>
             </div>
