eec281db7830a4509957d574afa675bcf763db85
angelosoreta123 <angelosoreta12.as@gmail.com>
2024-04-23 11:25:00 +0800

    wews

--- src/components/DeficientSubj/Deficientsub.jsx
+++ src/components/DeficientSubj/Deficientsub.jsx
@@ -8,30 +8,7 @@ const Deficientsub = () => {
         <h2>My Deficient Subjects</h2>
         <h3>See All</h3>
       </div>
-      <div className="card3lower">
-        <div className="sub1"></div>
-        <div className="sub2"></div>
-        <div className="sub3"></div>
-        <div className="sub4"></div>
-        <div className="sub5"></div>
-      </div>
-      <div className="subjname">
-        <div className="subname1">
-          <h4>Subject Name</h4>
-        </div>
-        <div className="subname2">
-          <h4>Subject Name</h4>
-        </div>
-        <div className="subname3">
-          <h4>Subject Name</h4>
-        </div>
-        <div className="subname4">
-          <h4>Subject Name</h4>
-        </div>
-        <div className="subname5">
-          <h4>Subject Name</h4>
-        </div>
-      </div>
+      <div className="card3lower"></div>
     </div>
   );
 };
--- src/components/Pages/Subjects/Subjects.jsx
+++ src/components/Pages/Subjects/Subjects.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import "./Subjects.css";
 
 const Subjects = ({ name, subjectcode, onClick }) => {
