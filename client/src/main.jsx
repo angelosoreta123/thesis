646d21a2a6b6c77e3c5d4a75c257d17af0b065b2
angelosoreta123 <angelosoreta12.as@gmail.com>
2024-04-29 16:41:47 +0800

    latest

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import { ContextProvider } from "../context/ContextProvider";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Semester from "./pages/Semester/Semester";
 import Curriculum from "./pages/Curriculum/Curriculum";
@@ -9,49 +16,64 @@ import Login from "./pages/AccManage/Login";
 import Register from "./pages/AccManage/Register";
 import Forgot from "./pages/AccManage/Forgot";
 import Subjectstab from "./pages/Subjects/Subjectstab";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8000";
-axios.defaults.withCredentials = true;
 
 const router = createBrowserRouter([
   {
-    path: "/Dashboard",
-    element: <Dashboard />,
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/Dashboard" />,
+      },
+      {
+        path: "/Dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/Subjects",
+        element: <Subjectstab />,
+      },
+      {
+        path: "/Semester",
+        element: <Semester />,
+      },
+      {
+        path: "/Curriculum",
+        element: <Curriculum />,
+      },
+      {
+        path: "/Profilepage",
+        element: <Profilepage />,
+      },
+    ],
   },
-  {
-    path: "/Subjects",
-    element: <Subjectstab />,
-  },
-  {
-    path: "/Semester",
-    element: <Semester />,
-  },
-  {
-    path: "/Curriculum",
-    element: <Curriculum />,
-  },
-  {
-    path: "/Profilepage",
-    element: <Profilepage />,
-  },
+
   {
     path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/Register",
-    element: <Register />,
-  },
-  {
-    path: "/Forgot",
-    element: <Forgot />,
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+      },
+      {
+        path: "/Forgot",
+        element: <Forgot />,
+      },
+    ],
   },
 ]);
 
 // Render the Main component within React.StrictMode using createRoot
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </React.StrictMode>
 );
